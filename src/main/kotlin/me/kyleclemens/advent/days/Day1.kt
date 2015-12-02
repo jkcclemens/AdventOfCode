2bf41f3bf0f2b561aca7755cdf727243ccdc1271
Kyle Clemens <jkc.clemens@gmail.com>
2015-12-02 12:12:06 -0500

    Added: Comments
    Also fixed bad index

--- src/main/kotlin/me/kyleclemens/advent/days/Day1.kt
+++ src/main/kotlin/me/kyleclemens/advent/days/Day1.kt
@@ -8,20 +8,29 @@ import me.kyleclemens.advent.helpers.UsesData
 class Day1 : DataCarrying, ProducesAnswers {
 
     private fun produceFirstAnswer(): String {
+        // Get the number of increments
         val increments = this.getData().replace(")", "").length
+        // Get the number of decrements
         val decrements = this.getData().replace("(", "").length
+        // Subtracting the decrements from the increments will indicate the floor Santa has to go to.
         return (increments - decrements).toString()
     }
 
     private fun produceSecondAnswer(): String {
+        // Start our count at 0
         var count = 0
+        // Loop through every instruction in the data with its index
         for (c in this.getData().withIndex()) {
+            // If it's an increment instruction, increment the count
             if (c.value == '(') count++
+            // If it's a decrement instruction, decrement the count
             else if (c.value == ')') count--
+            // If the count is -1 (the basement), return the current index plus one (starts at one)
             if (count == -1) {
-                return c.index.toString()
+                return (c.index + 1).toString()
             }
         }
+        // If the count never equals -1, the basement was never entered, indicating invalid data
         throw IllegalStateException("The basement is never entered")
     }
 
--- src/main/kotlin/me/kyleclemens/advent/days/Day2.kt
+++ src/main/kotlin/me/kyleclemens/advent/days/Day2.kt
@@ -10,31 +10,42 @@ class Day2 : DataCarrying, ProducesAnswers {
     private data class Dimensions(val length: Int, val width: Int, val height: Int)
 
     private fun String.toDimensions(): Dimensions {
+        // The string should have two 'x's if it is a valid Dimensions
         require(this.count { it == 'x' } == 2)
+        // Split the string by 'x', convert the results to ints, then convert them to a Dimensions object
         return this.split('x').map { it.toInt() }.run { Dimensions(this[0], this[1], this[2]) }
     }
 
     private fun getDimensions(): List<Dimensions> {
+        // Get the data for this day, split it by lines, filter out empty lines, convert each line to a Dimensions
         return this.getData().split("\n").filterNot { it.isEmpty() }.map { it.toDimensions() }
     }
 
     private fun produceFirstAnswer(): String {
         return this.getDimensions().map { dimension ->
+            // Do the necessary calculations
             val side1 = 2 * dimension.length * dimension.width
             val side2 = 2 * dimension.width * dimension.height
             val side3 = 2 * dimension.height * dimension.length
+            // Find the smallest and divide it by two to find the slack (these are all multiplied by two because the
+            // faces directly across from them are the same, and this is a 6-sided object)
             val smallest = listOf(side1, side2, side3).min()!! / 2
+            // Add all the surface area and the slack
             return@map side1 + side2 + side3 + smallest
-        }.sum().toString()
+        }.sum().toString() // sum the results
     }
 
     private fun produceSecondAnswer(): String {
         return this.getDimensions().map { dimension ->
+            // Find the volume
             val volume = dimension.height * dimension.width * dimension.length
+            // Do the necessary calculations
             val side1 = dimension.height * 2
             val side2 = dimension.length * 2
             val side3 = dimension.width * 2
+            // Check each perimeter and find the smallest one
             val smallest = listOf(side1 + side2, side1 + side3, side2 + side1, side2 + side3, side3 + side1, side3 + side2).min()!!
+            // Add the volume and the smallest perimeter
             return@map volume + smallest
         }.sum().toString()
     }
