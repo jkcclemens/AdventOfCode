dbf7dab6d6426fca9a6cee3791a5c085b91b65ca
Kyle Clemens <jkc.clemens@gmail.com>
2015-12-04 02:56:30 -0500

    Updated: Comments

--- src/main/kotlin/me/kyleclemens/advent/days/Day4.kt
+++ src/main/kotlin/me/kyleclemens/advent/days/Day4.kt
@@ -8,14 +8,23 @@ import kotlin.text.Regex
 @UsesData(day = 4)
 open class Day4 : Solution {
 
+    /**
+     * Find the number necessary to append to the key to generate an MD5 hash starting with [num] zeroes.
+     */
     private fun findNumberWithZeroes(num: Int): Int {
+        // Make a string of zeroes equal to the length of num
         val goal = "".padEnd(num, '0')
+        // Get the key to append to
         val key = this.getData().replace(Regex("\r?\n"), "")
+        // Start at -1
         var i = -1
         while (true) {
-            val firstThree = MessageDigest.getInstance("MD5").digest("$key${++i}".toByteArray()).take(Math.ceil(num / 2.0).toInt())
-            val firstFive = firstThree.map { Integer.toHexString(it.toInt()).padStart(2, '0') }.joinToString("").substring(0, num)
-            if (firstFive == goal) {
+            // Increment i, get the MD5, take the necessary number of bytes
+            val bytes = MessageDigest.getInstance("MD5").digest("$key${++i}".toByteArray()).take(Math.ceil(num / 2.0).toInt())
+            // Turn the bytes into a hex string and substring it to num length
+            val result = bytes.map { Integer.toHexString(it.toInt()).padStart(2, '0') }.joinToString("").substring(0, num)
+            if (result == goal) {
+                // If the result is equal to our goal, return the number
                 return i
             }
         }
