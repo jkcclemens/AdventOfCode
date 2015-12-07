be39d9b4a127a2640ef6917588eae55661eb53ff
Kyle Clemens <jkc.clemens@gmail.com>
2015-12-06 21:40:47 -0500

    Updated: Use better code

--- src/main/kotlin/me/kyleclemens/advent/days/Day6.kt
+++ src/main/kotlin/me/kyleclemens/advent/days/Day6.kt
@@ -7,19 +7,32 @@ import kotlin.text.Regex
 @UsesData(day = 6)
 open class Day6 : Solution {
 
-    private class Grid {
-        val grid = Array(1000, { BooleanArray(1000) })
+    private abstract class Grid<T> {
+        abstract val grid: Array<Array<T>>
+        abstract fun processInstruction(instruction: Instruction, value: T): T
 
         fun processInstruction(instructionWithPoints: InstructionWithPoints) {
             val p1 = instructionWithPoints.pointOne
             val p2 = instructionWithPoints.pointTwo
             for (x in p1.x..p2.x) {
                 for (y in p1.y..p2.y) {
-                    this.grid[x][y] = instructionWithPoints.instruction.process(this.grid[x][y])
+                    this.grid[x][y] = this.processInstruction(instructionWithPoints.instruction, this.grid[x][y])
                 }
             }
         }
 
+    }
+
+    private class OnOffGrid : Grid<Boolean>() {
+        override val grid = Array(1000, { BooleanArray(1000).toTypedArray() })
+
+        override fun processInstruction(instruction: Instruction, value: Boolean) = instruction.process(value)
+    }
+
+    private class BrightnessGrid : Grid<Int>() {
+        override val grid = Array(1000, { IntArray(1000).toTypedArray() })
+
+        override fun processInstruction(instruction: Instruction, value: Int) = instruction.process(value)
     }
 
     private data class Point(val x: Int, val y: Int)
@@ -49,86 +62,45 @@ open class Day6 : Solution {
             override fun process(boolean: Boolean): Boolean {
                 return false
             }
+
+            override fun process(int: Int): Int {
+                return Math.max(0, int - 1)
+            }
         },
         ON {
             override fun process(boolean: Boolean): Boolean {
                 return true
+            }
+
+            override fun process(int: Int): Int {
+                return int + 1
             }
         },
         TOGGLE {
             override fun process(boolean: Boolean): Boolean {
                 return !boolean
             }
+
+            override fun process(int: Int): Int {
+                return int + 2
+            }
         };
 
         abstract fun process(boolean: Boolean): Boolean
+        abstract fun process(int: Int): Int
     }
 
     private fun produceFirstAnswer(): Int {
-        val grid = Grid()
+        val grid = OnOffGrid()
         this.data.split("\n").filterNot { it.isEmpty() }.map { it.toInstruction() }.forEach { grid.processInstruction(it) }
         return grid.grid.sumBy { it.count { boolean -> boolean } }
     }
 
     // Second part
 
-    private class GridWithBrightness {
-        val grid = Array(1000, { IntArray(1000) })
-
-        fun processInstruction(instructionWithPoints: BrightnessInstructionWithPoints) {
-            val p1 = instructionWithPoints.pointOne
-            val p2 = instructionWithPoints.pointTwo
-            for (x in p1.x..p2.x) {
-                for (y in p1.y..p2.y) {
-                    this.grid[x][y] = instructionWithPoints.instruction.process(this.grid[x][y])
-                }
-            }
-        }
-    }
-
-    private data class BrightnessInstructionWithPoints(val instruction: BrightnessInstruction, val pointOne: Point, val pointTwo: Point)
-
-    private fun String.toBrightnessInstruction(): BrightnessInstructionWithPoints {
-        val instruction = when {
-            this.startsWith("turn off") -> BrightnessInstruction.OFF
-            this.startsWith("turn on") -> BrightnessInstruction.ON
-            this.startsWith("toggle") -> BrightnessInstruction.TOGGLE
-            else -> throw IllegalStateException("Invalid instruction")
-        }
-        val halfMarker = this.indexOf(' ') + this.indexOf(',')
-        val pointOne = this.substring(0, halfMarker).replace(Regex("[^\\d,]"), "").split(",")
-        val secondHalf = this.substring(halfMarker + 1, this.length)
-        val pointTwo = secondHalf.substring(secondHalf.indexOf(' ') + 1, secondHalf.length).split(",")
-        return BrightnessInstructionWithPoints(
-            instruction,
-            Point(pointOne[0].toInt(), pointOne[1].toInt()),
-            Point(pointTwo[0].toInt(), pointTwo[1].toInt())
-        )
-    }
-
-    private enum class BrightnessInstruction {
-        OFF {
-            override fun process(int: Int): Int {
-                return Math.max(0, int - 1)
-            }
-        },
-        ON {
-            override fun process(int: Int): Int {
-                return int + 1
-            }
-        },
-        TOGGLE {
-            override fun process(int: Int): Int {
-                return int + 2
-            }
-        };
-
-        abstract fun process(int: Int): Int
-    }
-
     private fun produceSecondAnswer(): Int {
-        val grid = GridWithBrightness()
-        this.data.split("\n").filterNot { it.isEmpty() }.map { it.toBrightnessInstruction() }.forEach { grid.processInstruction(it) }
+        val grid = BrightnessGrid()
+        this.data.split("\n").filterNot { it.isEmpty() }.map { it.toInstruction() }.forEach { grid.processInstruction(it) }
         return grid.grid.sumBy { it.sum() }
     }
 
