7cc687feb6566f88675d40ba5d90d6eab5411f62
Kyle Clemens <jkc.clemens@gmail.com>
2015-12-07 14:58:32 -0500

    Updated: Output times

--- src/main/kotlin/me/kyleclemens/advent/Main.kt
+++ src/main/kotlin/me/kyleclemens/advent/Main.kt
@@ -7,13 +7,21 @@ import me.kyleclemens.advent.days.Day4
 import me.kyleclemens.advent.days.Day5
 import me.kyleclemens.advent.days.Day6
 import me.kyleclemens.advent.days.Day7
+import me.kyleclemens.advent.helpers.Solution
+
+private fun timeDay(day: Int, solution: Solution) {
+    val start = System.currentTimeMillis()
+    val answers = solution.answers
+    val diff = System.currentTimeMillis() - start
+    println("Day $day: $answers – ${diff}ms")
+}
 
 fun main(args: Array<String>) {
-    println("Day 1: ${Day1().answers}")
-    println("Day 2: ${Day2().answers}")
-    println("Day 3: ${Day3().answers}")
-    println("Day 4: ${Day4().answers}") // takes a while
-    println("Day 5: ${Day5().answers}")
-    println("Day 6: ${Day6().answers}")
-    println("Day 7: ${Day7().answers}")
+    timeDay(1, Day1())
+    timeDay(2, Day2())
+    timeDay(3, Day3())
+    timeDay(4, Day4()) // takes a while
+    timeDay(5, Day5())
+    timeDay(6, Day6())
+    timeDay(7, Day7())
 }
