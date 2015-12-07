56c7b0fe997aebad2cd1531f0b4a480269e89a9a
Kyle Clemens <jkc.clemens@gmail.com>
2015-12-07 10:04:07 -0500

    Updated: Replace b

--- src/main/kotlin/me/kyleclemens/advent/days/Day7.kt
+++ src/main/kotlin/me/kyleclemens/advent/days/Day7.kt
@@ -8,9 +8,19 @@ import kotlin.text.Regex
 open class Day7 : Solution {
 
     private class BitwiseFun {
+        /**
+         * Mutable list of operations to apply
+         */
         val operations = arrayListOf<Operation>()
+        /**
+         * Map of wires, names and values.
+         */
         val wires = hashMapOf<String, Int>()
 
+        /**
+         * Processes all operations in [operations] once. If `true` is returned by an operation, it is removed from the
+         * list. If false is returned, it is kept in the list to be run again.
+         */
         fun process() {
             val iterator = this.operations.iterator()
             while (iterator.hasNext()) {
@@ -20,10 +30,16 @@ open class Day7 : Solution {
             }
         }
 
+        /**
+         * Adds an operation to [operations].
+         */
         fun addOperation(operation: Operation) {
             this.operations += operation
         }
 
+        /**
+         * Runs [process] until [operations] is empty.
+         */
         fun processUntilEmpty() {
             while (this.operations.isNotEmpty()) {
                 this.process()
@@ -42,6 +58,19 @@ open class Day7 : Solution {
         }
     }
 
+    /**
+     * Processes a gate.
+     * @param[left] The left side of the instruction
+     * @param[right] The right side of the instruction
+     * @param[wires] The wires, names and values
+     * @param[regex] The regular expression that [left] must match.
+     * @param[function] The function to invoke with a list of wire values matched by the regular expression (groups).
+     *                  This should return the value of the wire specified by [right]. This is only invoked if all
+     *                  groups match and every wire mentioned is in [wires].
+     * @return Returns null if the regex does not match or if all groups do not match. Returns false if not all wires
+     *         are in [wires]. Otherwise, returns true after setting [right] in [wires] to the value returned by
+     *         [function].
+     */
     private fun gate(left: String, right: String, wires: MutableMap<String, Int>, regex: Regex, function: (List<Int>) -> Int): Boolean? {
         val match = regex.matchEntire(left) ?: return null
         val groups = match.groups.map { it?.value }.let { it.takeLast(it.size - 1) }
@@ -51,6 +80,9 @@ open class Day7 : Solution {
         return true
     }
 
+    /**
+     * Converts this string to an [Operation].
+     */
     private fun String.toOperation(): Operation {
         val (left, right) = this.split(" -> ")
         return Operation { wires ->
@@ -64,7 +96,7 @@ open class Day7 : Solution {
             gate(left, right, wires, Regex("(\\w+) RSHIFT (\\w+)")) { comps -> comps[0] shr comps[1] }?.let { return@Operation it }
             gate(left, right, wires, Regex("NOT (\\w+)")) { comps -> comps[0].inv() and 0xFFFF }?.let { return@Operation it }
             if (left !in wires) {
-                return@Operation right in wires
+                return@Operation false
             }
             wires[right] = wires[left]!!
             return@Operation true
@@ -80,7 +112,7 @@ open class Day7 : Solution {
 
     private fun produceSecondAnswer(): Int {
         val bitwiseFun = BitwiseFun()
-        ("${this.produceFirstAnswer()} -> b\n" + this.data).split("\n").filterNot { it.isEmpty() }.map { it.toOperation() }.forEach { bitwiseFun.addOperation(it) }
+        this.data.replace(Regex("\\d+ -> b\n"), "${this.produceFirstAnswer()} -> b\n").split("\n").filterNot { it.isEmpty() }.map { it.toOperation() }.forEach { bitwiseFun.addOperation(it) }
         bitwiseFun.processUntilEmpty()
         return bitwiseFun.wires["a"]!!
     }
